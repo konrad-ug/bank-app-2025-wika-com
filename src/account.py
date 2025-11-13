e5bf8000ccc0fdf2a7cbfac56e2ae79d918c5ba5
Wiktoria <wworonecka@gmail.com>
2025-11-13 13:49:35 +0100

    ok

--- src/account.py
+++ src/account.py
@@ -3,6 +3,7 @@ class Account:
     def __init__(self, first_name, last_name,pesel):
         self.first_name = first_name
         self.last_name = last_name
+        self.historia = []
         self.balance = 0.0
         self.pesel = pesel if self.is_pesel_valid(pesel) else "Invalid"
     def is_pesel_valid(self,pesel):
@@ -13,15 +14,33 @@ class Account:
     def przelew_przych(self,kwota):
         if kwota>0.0:
             self.balance=self.balance+kwota
-            return self.balance
+            self.historia.append(kwota)
+            return self.balance, self.historia
     def przelew_wych(self,kwota,typ):
         if typ=="n":
             if kwota>0.0 and kwota-self.balance<=0.0:
                 self.balance=self.balance-kwota
-                return self.balance
+                self.historia.append(kwota*-1)
+                return self.balance, self.historia
         elif typ=="e":
             if kwota>0.0 and kwota-self.balance<=0.0:
-                self.balance=self.balance-kwota-1
-                return self.balance
+                oplata=1
+                self.balance=self.balance-kwota-oplata
+                self.historia.append(kwota*(-1))
+                self.historia.append(-oplata)
+                return self.balance, self.historia
             else:
                 return "nieudany przelew"
+    def submit_for_loan(self,kwota):
+        if len(self.historia)>2:
+            for i in range(1,4):
+                if self.historia[-i]<0:
+                    if len(self.historia)>4:
+                        for i in range(1,6):
+                            suma=suma+self.historia[i]
+                        if suma>kwota:
+                            return True
+                    return False
+            return True
+        else:
+            return False
--- tests/unit/test_przelewy.py
+++ tests/unit/test_przelewy.py
@@ -9,11 +9,30 @@ class TestPrzelew:
         assert account.balance == 320.0
     def przelew_wy(self):
         account = Account("John", "Doe","12345678910")
-        ccount.przelew_przych(320)
-        account.przelew_wych(200) 
+        account.przelew_przych(320)
+        account.przelew_wych(200,"n") 
         assert account.balance == 120.0
     def przelew_wy_ekspres(self):
         account = Account("John", "Doe","12345678910")
-        ccount.przelew_przych(320)
+        account.przelew_przych(320)
         account.przelew_wych(200,"e") 
         assert account.balance == 119.0
+
+class TestHist:
+    def test_hist_przych(self):
+        account=Account("John", "Doe","12345678910")
+        account.przelew_przych(120)
+        account.przelew_przych(200)
+        assert account.historia == [120,200]
+    def test_hist_wych(self):
+        account=Account("John", "Doe","12345678910")
+        account.przelew_przych(200)
+        account.przelew_przych(120)
+        account.przelew_wych(200,"n") 
+        assert account.historia == [200,120,-200]
+    def test_hist_wych_e(self):
+        account=Account("John", "Doe","12345678910")
+        account.przelew_przych(200)
+        account.przelew_przych(120)
+        account.przelew_wych(200,"e") 
+        assert account.historia == [200,120,-200,-1]
