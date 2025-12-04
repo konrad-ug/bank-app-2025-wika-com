4c722f180b4947728920d8239ec1110f66712f3d
Wiktoria <wworonecka@gmail.com>
2025-12-04 01:26:53 +0100

    ok

--- src/accountsRegistry.py
+++ src/accountsRegistry.py
@@ -7,4 +7,14 @@ class AccountRegistry:
     def add_account(self, account: PersonalAccount):
         self.accounts.append(account)
 
-    
+    def find_by_pesel(self, pesel):
+        for account in self.accounts:
+            if account.pesel == pesel:
+                return account
+        return None
+
+    def get_all_accounts(self):
+        return self.accounts
+
+    def count_accounts(self):
+        return len(self.accounts)
