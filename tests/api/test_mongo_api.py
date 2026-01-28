3677e33f3973f9123387778f9d5070ee010c305d
Wiktoria <wworonecka@gmail.com>
2026-01-28 18:28:36 +0100

    koniec

--- tests/api/test_mongo_api.py
+++ tests/api/test_mongo_api.py
@@ -10,20 +10,6 @@ def client():
     with app.test_client() as client:
         yield client
 
-# def test_api_save_and_load(client):
-#     client.post("/api/accounts", json={
-#         "first_name": "Adam", "last_name": "Nowak", "pesel": "11111111111"
-#     })
-
-#     save_resp = client.post("/api/accounts/save")
-#     assert save_resp.status_code == 200
-#     load_resp = client.post("/api/accounts/load")
-#     assert load_resp.status_code == 200
-#     assert "Pomyślnie załadowano" in load_resp.get_json()["message"]
-#     get_resp = client.get("/api/accounts/11111111111")
-#     assert get_resp.status_code == 200
-#     assert get_resp.get_json()["first_name"] == "Adam"
-
 def test_api_save_and_load(client, mocker):
     mock_repo = mocker.patch("app.api.repo")
     client.post("/api/accounts", json={
